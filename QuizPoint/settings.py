8fefae5197a2454adafdda346942c0a0d8b43fe0
brenner87 <alk870501@gmail.com>
2018-03-31 15:12:09 +0300

    troubleshooting

--- QuizPoint/settings.py
+++ QuizPoint/settings.py
@@ -44,7 +44,7 @@ else:
         SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
